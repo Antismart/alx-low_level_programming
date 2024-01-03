e8ce4a33ef2851fbf87c70c556c6819dcc4f9893
Antismart <timbwamoses83@gmail.com>
2024-01-03 14:44:53 +0300

    remove extra space

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,11 @@
  */
 void _puts(char *str)
 {
-	 while (*str != '\0')
-	 {
-		 write(1, str, 1);
-		 str++;
-	 }
-	 write(1, "\n", 1);
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
 }
 

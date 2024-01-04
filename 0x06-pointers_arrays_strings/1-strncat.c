2d03d17973d83f5f5abb34f57d0fbe5cdc5e11a0
Antismart <timbwamoses83@gmail.com>
2024-01-04 16:02:56 +0300

    Implemented _strncat function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of bytes to concatenate from src
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len++] = src[i];
+
+	dest[dest_len] = '\0';
+
+	return dest;
+}

934c40447acc5887ccefb29cd040b220905322cf
Antismart <timbwamoses83@gmail.com>
2024-01-13 16:26:29 +0300

    implemented a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - calculates sum of two numbers
+ * @argc: argument count
+ * @argv: argument velocity
+ *
+ * Return: 0 success, 1- fail
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int mul, num1, num2;
+
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+
+	return (0);
+}

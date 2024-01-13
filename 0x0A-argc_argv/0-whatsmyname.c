66c6d5617e2d6b77c4c2807fa3092bf88c837d42
Antismart <timbwamoses83@gmail.com>
2024-01-13 16:18:16 +0300

    implemented a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: size of array command line arg.
+ * @argv: command line argument
+ *
+ * Return: Always 0, success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

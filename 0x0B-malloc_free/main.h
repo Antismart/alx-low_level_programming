516cea200cc5d6a6d7f63f7de230b493d2883e03
Antismart <timbwamoses83@gmail.com>
2024-01-17 22:23:10 +0300

    added main.h file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *_strdup(char *str);
+int _strlen(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+
+#include <stdio.h>
+#include <stdlib.h>

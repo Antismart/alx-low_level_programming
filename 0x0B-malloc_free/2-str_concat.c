9c29d839876650f0cb3e267601efa0da1e243dce
Antismart <timbwamoses83@gmail.com>
2024-01-17 22:32:29 +0300

    implement a function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,72 @@
+
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string whose length will be returned
+ * Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+/**
+ * str_concat - concatenates two strings and allocates new memory
+ * @s1: The first string passed
+ * @s2: The second string passed
+ * Return: length of string
+ */
+
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, j, size;
+
+	i = 0;
+	j = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	size = _strlen(s1) + _strlen(s2) + 1;
+
+	s = malloc(sizeof(char) * size);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		s[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	s[size] = '\0';
+
+	return (s);
+}

9c380aa85ed57af85ab2c8d59cd91da43a6de3d5
Antismart <timbwamoses83@gmail.com>
2024-01-17 22:26:53 +0300

    impliment a function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - Create an array filled with a char.
+ * @size: size of array to be created
+ * @c: character to fill the array
+ * Return: a pointer to the array creater or Null if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
